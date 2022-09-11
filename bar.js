8f23af40f69136431933dae93039bb8a96a5b0fb
Mashrur Rahman <mashrur.rahman@criticalmass.com>
2022-09-11 15:37:25 -0400

    added horizontal bar chart

--- bar.js
+++ bar.js
@@ -16,13 +16,13 @@ const data = {
   datasets: [
     {
       label: "Total Yearly sales",
-      fill: true,
       data: [95, 129, 170, 116, 96, 274, 236, 43, 262, 399, 269, 181],
       backgroundColor: "rgba(54, 162, 235, 0.5)",
       borderColor: "rgba(54, 162, 235, 0.9)",
       borderWidth: 2,
       borderRadius: 10,
       borderSkipped: false,
+      // axis: 'y',
     },
   ],
 };
@@ -30,6 +30,10 @@ const data = {
 const config = {
   type: "bar",
   data: data,
+  // To build horizotal bar chart
+  // options: {
+  //   indexAxis: "y",
+  // },
 };
 
 new Chart(ctx, config);
